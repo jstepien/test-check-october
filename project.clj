42b504a64afa435dbb866d441d68335c60a52dda
Azel4231 <Azel4231@users.noreply.github.com>
2017-10-28 13:36:58 +0200

    Use spec for property based testing

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject test-check-october "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
                 [org.clojure/test.check "0.9.0"]])
--- src/test_check_october/core.clj
+++ src/test_check_october/core.clj
@@ -1,4 +1,6 @@
-(ns test-check-october.core)
+(ns test-check-october.core
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.test.alpha :as st]))
 
 (defn steps-per-hour
   [steps seconds]
@@ -6,5 +8,34 @@
     (throw (java.lang.ArithmeticException.))
     (/ (* 3600.0 steps) seconds)))
 
+
 (defn jan-reverse [coll]
   (reverse coll))
+
+(defn first-is-last [{ret :ret {coll :coll} :args}]
+  (= (first ret)
+     (last coll)))
+
+(defn retain-count [{ret :ret {coll :coll} :args}]
+  (= (count ret)
+     (count coll)))
+
+(defn retain-elements [{ret :ret {coll :coll} :args}]
+  (= (set ret)
+     (set coll)))
+
+(defn involution [{ret :ret {coll :coll} :args}]
+  (= (jan-reverse ret)
+     coll))
+
+(s/fdef jan-reverse
+        :args (s/cat :coll sequential?)
+        :ret sequential?
+        :fn (s/and first-is-last
+                   retain-count
+                   retain-elements
+                   involution))
+
+(st/instrument)
+#_(jan-reverse 5)
+
